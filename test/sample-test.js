05d1a26ba30ea69140cc8322105f4acbde41bfb8
noah figueras <noahfigueras@gmail.com>
2021-12-20 00:45:16 -0800

    Tests of contracts call passed successfully

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,25 +1,63 @@
 const { expect } = require("chai");
 
 describe("Safe Transfer Reciever Contract", function() {
-  it("ERC721 should hit contract and transfer to vault", async function() {
-    const Contract = await ethers.getContractFactory("SmartContract");
-    const contract = await Contract.deploy();
+  let Contract;
+  let contract;
+  let Nft_mock;
+  let nft_mock;
+  let ERC1155_MOCK;
+  let erc1155_mock;
+  let id;
+  let owner;
+  let vault;
+
+  beforeEach( async () => {
+    Contract = await ethers.getContractFactory("SmartContract");
+    contract = await Contract.deploy();
     await contract.deployed();
 
-    const Nft_mock = await ethers.getContractFactory("MyERC721");
-    const nft_mock = await Nft_mock.deploy();
+    Nft_mock = await ethers.getContractFactory("MyERC721");
+    nft_mock = await Nft_mock.deploy();
     await nft_mock.deployed();
+    
+    ERC1155_MOCK = await ethers.getContractFactory("MyERC1155");
+    erc1155_mock = await ERC1155_MOCK.deploy();
+    await erc1155_mock.deployed();
 
-    const id = 1;
-    const [owner, vault] = await ethers.getSigners();
+    id = 1;
+    [owner, vault] = await ethers.getSigners();
+  });
 
+  it("ERC721 should hit contract and transfer to vault", async function() {
     await contract.recieve({value: ethers.utils.parseUnits("1.0")});
     await contract.setVault(vault.address);
 
     const transfer = await nft_mock['safeTransferFrom(address,address,uint256)'](owner.address,contract.address,id); 
     const tx = await transfer.wait();
-    console.log('NFT Transfer succesfull');
 
     expect(await nft_mock.ownerOf(id)).to.equal(vault.address);
+  });
+
+  it("ERC115 single should hit contract and transfer to vault", async function() {
+    await contract.recieve({value: ethers.utils.parseUnits("1.0")});
+    await contract.setVault(vault.address);
+
+    const transfer = await erc1155_mock.safeTransferFrom(owner.address,contract.address,id,1,ethers.utils.arrayify("0x")); 
+    const tx = await transfer.wait();
+
+    expect(await erc1155_mock.balanceOf(vault.address,id)).to.equal(1);
+  });
+
+  it("ERC115 batch should hit contract and transfer to vault", async function() {
+    await contract.recieve({value: ethers.utils.parseUnits("1.0")});
+    await contract.setVault(vault.address);
+
+    const transfer = await erc1155_mock.safeBatchTransferFrom(owner.address,contract.address,[0,1,2],[2,4,1],ethers.utils.arrayify("0x")); 
+    const tx = await transfer.wait();
+
+    expect(await erc1155_mock.balanceOf(vault.address,0)).to.equal(2);
+    expect(await erc1155_mock.balanceOf(vault.address,1)).to.equal(4);
+    expect(await erc1155_mock.balanceOf(vault.address,2)).to.equal(1);
+    expect(await erc1155_mock.balanceOf(owner.address,2)).to.equal(0);
   });
 });
