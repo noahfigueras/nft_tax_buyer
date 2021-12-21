0313c835b09b47f9332627e2e587ec1af51747e9
noah figueras <noahfigueras@gmail.com>
2021-12-21 13:28:55 -0800

    Gas estimation successfull, ERC1155 BATCH needs to change impersonate account for transfer, not enough funds

--- scripts/estimateGas.js
+++ scripts/estimateGas.js
@@ -19,8 +19,8 @@ async function main() {
   await contract.setVault(vault.address);
 
   // GENERAL SETUP
-  const collection721 = "0x60e4d786628fea6478f785a6d7e704777c86a7c6";
-  const collection1155 = "0x28472a58a490c5e09a238847f66a68a47cc76f0f";
+  const collection721 = "0x1a92f7381b9f03921564a437210bb9396471050c";
+  const collection1155 = "0x0e1f990d264a9818460DDE1b81c0b04D8A88e751";
   const Abi721 = [
     'function safeTransferFrom(address from, address to, uint256 tokenId) external'
   ];
@@ -31,23 +31,27 @@ async function main() {
   // Impersonate Account
   await hre.network.provider.request({
       method: "hardhat_impersonateAccount",
-      params: ["0xb6f6b9aa69d6dfd8039b35f0c49463f27696d437"],
+      params: ["0x63989a803b61581683b54ab6188ffa0f4baadf28"],
   });
-  const signer = await ethers.getSigner("0xb6f6b9aa69d6dfd8039b35f0c49463f27696d437");
+  const signer = await ethers.getSigner("0x63989a803b61581683b54ab6188ffa0f4baadf28");
 
   // ERC721
   const contract721 = new ethers.Contract(collection721, Abi721, signer);
-  const transfer1 = await contract721.safeTransferFrom(signer.address, contract.address, 779); 
+  const transfer1 = await contract721.safeTransferFrom(signer.address, contract.address, 7748); 
   const tx1 = await transfer1.wait();
   console.log('ERC721 safeTransfer gas used:', Number(tx1.gasUsed));
 
   // ERC1155 Single
-  //  const contract1155 = new ethers.Contract(collection1155, Abi1155, signer);
-  // const transfer2 = await contract1155.safeTransferFrom(signer.address, contract.address, 0, 2, ethers.utils.arrayify("0x")); 
-  //const tx2 = await transfer2.wait();
-  //console.log('ERC1155 single safeTransfer gas used:', Number(tx2.gasUsed));
+  const contract1155 = new ethers.Contract(collection1155, Abi1155, signer);
+  const transfer2 = await contract1155.safeTransferFrom(signer.address, contract.address, 7, 2, ethers.utils.arrayify("0x")); 
+  const tx2 = await transfer2.wait();
+  console.log('ERC1155 single safeTransfer gas used:', Number(tx2.gasUsed));
 
   // ERC1155 BATCH 
+  const contract1155b = new ethers.Contract(collection1155, Abi1155, signer);
+  const transfer3 = await contract1155b.safeBatchTransferFrom(signer.address, contract.address, [7,9,1,2], [2,3,1,1], ethers.utils.arrayify("0x")); 
+  const tx3 = await transfer3.wait();
+  console.log('ERC1155 batch safeBatchTransfer gas used:', Number(tx3.gasUsed));
 }
 
 main()
