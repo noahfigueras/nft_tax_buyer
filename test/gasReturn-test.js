d22dfebcc31d517a1aa052a7938abbe73b109bab
noah figueras <noahfigueras@gmail.com>
2021-12-21 13:27:46 -0800

    all test pass correctly

--- test/gasReturn-test.js
+++ test/gasReturn-test.js
@@ -54,4 +54,45 @@ describe("Gas % returns correctly to caller", function() {
     const finalBalance = await owner.getBalance();
     expect(finalBalance).to.equal(pay.add(estimate));
   });
+
+  it("ERC1155 Single returns 10% of gas to caller", async function() {
+    const initBalance = await owner.getBalance();
+    const transfer = await erc1155_mock.safeTransferFrom
+    (
+      owner.address,
+      contract.address,
+      id, 
+      4,
+      ethers.utils.arrayify("0x")
+    ); 
+    const tx = await transfer.wait();
+
+    const txCost = tx.gasUsed.mul(transfer.gasPrice);
+    const balanceAfterGas = initBalance.sub(txCost); 
+    const pay = ethers.utils.parseUnits('1.0', 'gwei');
+    const gasReturn = ethers.BigNumber.from("8500");
+    const estimate = balanceAfterGas.add(gasReturn.mul(transfer.gasPrice));
+    const finalBalance = await owner.getBalance();
+    expect(String(finalBalance).slice(0,7)).to.equal(String(pay.add(estimate)).slice(0,7));
+  });
+  it("ERC1155 Batch returns 10% of gas to caller", async function() {
+    const initBalance = await owner.getBalance();
+    const transfer = await erc1155_mock.safeBatchTransferFrom
+    (
+      owner.address,
+      contract.address,
+      [0,1], 
+      [4,8],
+      ethers.utils.arrayify("0x")
+    ); 
+    const tx = await transfer.wait();
+
+    const txCost = tx.gasUsed.mul(transfer.gasPrice);
+    const balanceAfterGas = initBalance.sub(txCost); 
+    const pay = ethers.utils.parseUnits('1.0', 'gwei');
+    const gasReturn = ethers.BigNumber.from("13000");
+    const estimate = balanceAfterGas.add(gasReturn.mul(transfer.gasPrice));
+    const finalBalance = await owner.getBalance();
+    expect(String(finalBalance).slice(0,7)).to.equal(String(pay.add(estimate)).slice(0,7));
+  });
 });
