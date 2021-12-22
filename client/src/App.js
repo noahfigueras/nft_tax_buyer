0f6d8afc08f6b3ad1cfdb48aac55630f3598f84b
noah figueras <noahfigueras@gmail.com>
2021-12-22 15:26:52 -0800

    Added ERC1155 transfer single token with multiple items

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,10 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
-import React, { useState, useEffect} from 'react';
+import React, { useState } from 'react';
 
 function App() {
-	// Hooks
-  const [user, setUser] = useState([false, null]);
-
-	useEffect(() => {
-		// Connect to Metamask
-		if(window.ethereum.selectedAddress !== null) {
-			setUser([true, window.ethereum.selectedAddress]);
-		}
-	}, [user]);
+  const [user, setUser] = useState([]);
 
   return (
     <div className="App">
